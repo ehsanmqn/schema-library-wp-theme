1f10311a68d9653b9f39033158da1c86d62ff24e
Jasper Speicher <jasper.speicher@gmail.com>
2016-03-04 13:36:53 -0800

    Add Wordpress filter that allows developers to skip loading dependencies

--- omh-schema-library-functions.php
+++ omh-schema-library-functions.php
@@ -6,20 +6,30 @@
 
 function omh_schema_library_theme_scripts() {
 
-  //Theme css
-  wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css' );
-  wp_enqueue_style( 'angular-bootstrap', get_template_directory_uri() . '/bower_components/angular-bootstrap/ui-bootstrap-csp.css' );
-  wp_enqueue_style( 'codemirror', get_template_directory_uri() . '/bower_components/codemirror/lib/codemirror.css' );
-  wp_enqueue_style( 'omh-schema-library-style', get_template_directory_uri() . '/css/omh-schema-library-style.css' );
+  $load_deps = true;
 
-  // Theme js
-  wp_enqueue_script( 'angular', get_template_directory_uri() . '/bower_components/angular/angular.min.js', array() );
-  wp_enqueue_script( 'angular-bootstrap', get_template_directory_uri() . '/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js', array('angular') );
-  wp_enqueue_script( 'angular-ui-router', get_template_directory_uri() . '/bower_components/angular-ui-router/release/angular-ui-router.min.js', array('angular') );
-  wp_enqueue_script( 'moment', get_template_directory_uri() . '/bower_components/moment/min/moment.min.js', array() );
-  wp_enqueue_script( 'codemirror', get_template_directory_uri() . '/bower_components/codemirror/lib/codemirror.js', array() );
-  wp_enqueue_script( 'omh-documentation-utilities-script', get_template_directory_uri() . '/js/omh-documentation-utilities.js', array( 'jquery','moment' ) );
-  wp_enqueue_script( 'omh-schema-library-script', get_template_directory_uri() . '/js/omh-schema-library-functions.js', array( 'jquery','angular','moment','omh-documentation-utilities-script' ) );
+  if ( has_filter('load_schema_library_dependencies') ){
+    $load_deps = do_filter('load_schema_library_dependencies');
+  }
+
+  if ( $load_deps ){
+    //Theme css
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css' );
+    wp_enqueue_style( 'angular-bootstrap', get_template_directory_uri() . '/bower_components/angular-bootstrap/ui-bootstrap-csp.css' );
+    wp_enqueue_style( 'codemirror', get_template_directory_uri() . '/bower_components/codemirror/lib/codemirror.css' );
+    wp_enqueue_style( 'omh-schema-library-style', get_template_directory_uri() . '/css/omh-schema-library-style.css' );
+
+    // Theme js
+    wp_enqueue_script( 'angular', get_template_directory_uri() . '/bower_components/angular/angular.min.js', array() );
+    wp_enqueue_script( 'angular-bootstrap', get_template_directory_uri() . '/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js', array('angular') );
+    wp_enqueue_script( 'angular-ui-router', get_template_directory_uri() . '/bower_components/angular-ui-router/release/angular-ui-router.min.js', array('angular') );
+    wp_enqueue_script( 'moment', get_template_directory_uri() . '/bower_components/moment/min/moment.min.js', array() );
+    wp_enqueue_script( 'codemirror', get_template_directory_uri() . '/bower_components/codemirror/lib/codemirror.js', array() );
+    wp_enqueue_script( 'omh-documentation-utilities-script', get_template_directory_uri() . '/js/omh-documentation-utilities.js', array( 'jquery','moment' ) );
+  }
+
+  // Enqueue the schema lib js
+  wp_enqueue_script( 'omh-schema-library-script', get_template_directory_uri() . '/js/omh-schema-library-functions.js', array( 'jquery','angular','moment','omh-documentation-utilities-script' ), '0.1.1', false );
 
   // Localize the script with directory and schema data
   $schema_query = new WP_Query( array(
